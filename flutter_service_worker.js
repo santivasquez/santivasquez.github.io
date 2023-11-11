22e80666f1483bf55948f3c63afde4114359dfde
santiVasquez <santivaz852@hotmail.com>
2023-11-10 20:18:09 -0500

    WITHOPUT authentication

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "2b8971cb053b84ad42af0f04184f09f9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "35eac9ba9981286dc011eb034c38a751",
-"/": "35eac9ba9981286dc011eb034c38a751",
-"main.dart.js": "d5730faa32ff25d87600ec4e0f3f9750",
+"index.html": "2d3bb2e6ce64938f9a3f447b645e9863",
+"/": "2d3bb2e6ce64938f9a3f447b645e9863",
+"main.dart.js": "16b75a97a304a88a63df88accbe527ab",
 "manifest.json": "a782efd34a7f4232322c4e252eaecbd0",
 "version.json": "ef54a4ae329e507823db7566997fe9c3"};
 // The application shell files that are downloaded before a service worker can
