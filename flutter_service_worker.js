5b3383ecf4f5389ab9900f90576bd806f4ee0dc5
Santiago Vasquez <santivaz852@hotmail.com>
2024-05-29 20:32:57 -0500

    Contact icons and information in Page under construction

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "451af43c508f4c292f3ab8ec5af62a87",
-"assets/AssetManifest.bin.json": "d06148c0d5f7ed6f76459ee262fc3694",
-"assets/AssetManifest.json": "55b3c2c9d14530d8a6e7e7292f356934",
+const RESOURCES = {"assets/AssetManifest.bin": "ac52a55135ac36b84616b653a074528f",
+"assets/AssetManifest.bin.json": "7b809f19afc2baa9f4f085e799ee5736",
+"assets/AssetManifest.json": "ff0f02fa0fc3264cd61ee7b30bb80c9d",
 "assets/assets/data/genders.json": "d4bf17a856a42f5190002a94adf0d081",
 "assets/assets/data/relationships.json": "cb07e80f5f471266827c92818e014ea3",
 "assets/assets/data/topic_tastes.json": "a04dcfdea2acd127a6fc3b6480346c58",
@@ -17,10 +17,13 @@ const RESOURCES = {"assets/AssetManifest.bin": "451af43c508f4c292f3ab8ec5af62a87
 "assets/assets/images/No-Image-Placeholder.png": "8bb577688de585fe04c3b5e45ab4fd5e",
 "assets/assets/images/percentage_discount.png": "38ffaf782594cabb9ad60e096e663aa5",
 "assets/assets/images/springles_background.jpg": "c250032a101d456e02eeed5ae66c761f",
-"assets/FontManifest.json": "316a636bcc17db9bf8d0e5e78901c58b",
+"assets/FontManifest.json": "dcb1dfeaff398bc417d5442466c58834",
 "assets/fonts/MaterialIcons-Regular.otf": "4dd9e5393a5f83f3a01fbd13f79f68e9",
-"assets/NOTICES": "df7132f782d93172e8bba1b111cff589",
+"assets/NOTICES": "5e12d1e02a229d0217f1319d3c6981c5",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "1c1e84f5c1465c6cec89351be630140e",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "f3307f62ddff94d2cd8b103daf8d1b0f",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "04f83c01dded195a11d21c2edf643455",
 "assets/packages/social_media_flutter/fonts/icons.ttf": "7dae615e8df7bf5bf381bf713850ac33",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
@@ -39,9 +42,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "451af43c508f4c292f3ab8ec5af62a87
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4973c69347cf7b19a4f0236b931bb387",
-"/": "4973c69347cf7b19a4f0236b931bb387",
-"main.dart.js": "030d8824f4958e1abd4de7afa8095cc3",
+"index.html": "593f2a70e3e59807d7c0369e6986bdaa",
+"/": "593f2a70e3e59807d7c0369e6986bdaa",
+"main.dart.js": "6b7c365feb8152f7cd6e7ffa11324f65",
 "manifest.json": "a782efd34a7f4232322c4e252eaecbd0",
 "version.json": "ef54a4ae329e507823db7566997fe9c3"};
 // The application shell files that are downloaded before a service worker can
